e1433cc4413ccb8930a0d69a79573deaaef91209
yusufcanbircan <yusufcannnz@hotmail.com>
2023-04-15 14:21:39 +0300

    combine subscriber, publisher and assing usage

--- Frameworks/CombineFramework.playground/Contents.swift
+++ Frameworks/CombineFramework.playground/Contents.swift
@@ -2,6 +2,42 @@ import UIKit
 import Combine
 import PlaygroundSupport
 
+/// Publisher and Subscriber
+/// A publisher publishes values which you can receive from a subscriber.
+/// Publishers publish the objects one by one, and subscribers receive them one by one and perform its operators.
+///
+/// 'Just' keyword publishes just one object.
+let justPublisher = Just(25)
+
+///You need to subscribe to specific publisher to receive objects
+let justSubscription = justPublisher
+    .sink { receivedValue in
+        print(receivedValue)
+    }
+
+/// You can publish a sequence's values
+let sequencePublisher = ["Apple", "Google", "Microsoft", "Arabam.com"].publisher
+/// It publishes the values of the sequence one by one
+
+let sequenceSubscription = sequencePublisher /// receives each published value and performs the operations
+    .sink { sequenceValues in
+        print("The brand \(sequenceValues)")
+    }
+
+/// You can assign the published value to a property with assign operator.
+class AssignClass {
+    var property: String = "" {
+        didSet {
+            print("the new value of property \(property)")
+        }
+    }
+}
+
+var assignObject = AssignClass()
+var assignPublisher = ["Ali", "Cansu", "Yusuf", "Veli"].publisher
+var assignSubscription = assignPublisher.assign(to: \.property, on: assignObject)
+// to wants the property which you want to assign the values, on means the scope of property.
+
 
 /*PlaygroundPage.current.needsIndefiniteExecution = true
 
@@ -51,16 +87,16 @@ URLSession.shared.dataTaskPublisher(for: url)
     }
     .store(in: &cancellables)
 */
-
-let foodBank = ["Apple", "Orange", "Banana", "Lemon"].publisher
-
-var timer = Timer.publish(every: 1, on: .main, in: .common)
-    .autoconnect()
-
-let subscription = foodBank.zip(timer)
-    .sink { completion in
-        print("Completion \(completion)")
-    } receiveValue: { value in
-        print(value)
-    }
+//
+//let foodBank = ["Apple", "Orange", "Banana", "Lemon"].publisher
+//
+//var timer = Timer.publish(every: 1, on: .main, in: .common)
+//    .autoconnect()
+//
+//let subscription = foodBank.zip(timer)
+//    .sink { completion in
+//        print("Completion \(completion)")
+//    } receiveValue: { value in
+//        print(value)
+//    }
 
